6fad4dcc912fd6d6fd518543fee1baaa379a312d
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-03-23 06:02:19 +0700

    remove old code

--- src/data/schema.js
+++ src/data/schema.js
@@ -1,24 +1,21 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import {
   GraphQLSchema as Schema,
   GraphQLObjectType as ObjectType,
 } from 'graphql';
 
 import queries from './queries';
+import mutation from './mutation';
 
 const schema = new Schema({
   query: new ObjectType({
     name: 'RootQuery',
     fields: queries,
   }),
+  mutation: new ObjectType({
+    name: 'RootMutation',
+    fields: mutation,
+  }),
 });
 
 export default schema;
+
