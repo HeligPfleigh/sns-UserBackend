b02c40fa220c17d607476bf38a9952d90199507b
Linh Le Duc <linh.le@mttjsc.com>
2017-03-27 17:34:40 +0700

    Remove footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,7 +11,6 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Layout.css';
 import Header from '../Header';
-import Footer from '../Footer';
 
 class Layout extends React.Component {
   static propTypes = {
@@ -23,7 +22,6 @@ class Layout extends React.Component {
       <div>
         <Header />
         {this.props.children}
-        <Footer />
       </div>
     );
   }
