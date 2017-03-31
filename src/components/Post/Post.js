3738d27e8eb3eac743900756a02147dadc4abaaf
Linh Le Duc <linh.le@mttjsc.com>
2017-03-31 14:37:34 +0700

    Implement UI Post Component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,19 +1,46 @@
 import React, { PropTypes } from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Row, Col, Clearfix } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Post.css';
 
 const Post = ({ data: { _id, message, user } }) => (
-  <div className={s.title}>
-    <hr />
-    {`message: ${message}`}<br />
-    <div>
-      user<br />
+  <div className={s.postPanel}>
+    <Col className={s.postHeaderLeft}>
       <span className="hide">{_id}</span>
-      <Image src={user.profile.picture} rounded /><br />
-      firstName: {user.profile.firstName}<br />
-      lastName: {user.profile.lastName}<br />
-      gender: {user.profile.gender}<br />
+      <a href="#">
+        <Image src={user.profile.picture} circle />
+      </a>
+      <a href="#">
+        <strong>{`${user.profile.firstName} ${user.profile.lastName}`}</strong>
+      </a>
+    </Col>
+    <Col className={s.postHeaderRight}>
+      <i className="fa fa-angle-down fa-lg" aria-hidden="true"></i>
+    </Col>
+    <Clearfix />
+    <Col className={s.postContent}>
+      {`message: ${message}`}
+    </Col>
+    <Col className={s.postStatistic}>
+      <a href="#">16 Thích</a>
+      <a href="#">6 Bình luận</a>
+    </Col>
+    <Col className={s.postControl}>
+      <a href="#">
+        <i className="fa fa-heart-o fa-lg" aria-hidden="true"></i>&nbsp;
+        <span>Thích</span>
+      </a>
+      <a href="#">
+        <i className="fa fa-comments-o fa-lg" aria-hidden="true"></i>&nbsp;
+        <span>Bình luận</span>
+      </a>
+
+      <a href="#">
+        <i className="fa fa-share fa-lg" aria-hidden="true"></i>&nbsp;
+        <span>Chia sẻ</span>
+      </a>
+    </Col>
+    <div>
     </div>
   </div>
 );
