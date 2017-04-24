b79014e566505f2d7f41a4b241bee5534795a979
Nam Hoang Van (particle4dev) <particle4dev@gmail.com>
2017-04-24 17:59:39 +0700

    delete a lot of unused files

--- src/core/chat/index.js
+++
@@ -1,4 +0,0 @@
-import serviceChatFactory, { FirebaseProvider } from './services';
-import { auth } from '../../config';
-
-export default serviceChatFactory.createClient(FirebaseProvider, auth.firebase);
