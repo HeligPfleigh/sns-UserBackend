49abb64d63494adffc8e2ee10550eb1527be7659
Linh Le Duc <linh.le@mttjsc.com>
2017-05-16 09:50:54 +0700

    Change mutation createNewPost with case post on friend timelines and add trigger this task

--- src/data/mutation/posts/createNewPost.js
+++ src/data/mutation/posts/createNewPost.js
@@ -1,22 +1,31 @@
 import {
   GraphQLString as StringType,
 } from 'graphql';
+import _ from 'lodash';
 
 import PostSchemas from '../../schemas/PostSchemas';
+import { sendPostNotification } from '../../../utils/notifications';
 import { PostsModel } from '../../models';
 
 const createNewPost = {
   type: PostSchemas,
   args: {
     message: { type: StringType },
+    userId: { type: StringType },
   },
-  resolve: ({ request }, { message }) => new Promise(async (resolve, reject) => {
+  resolve: ({ request }, { message, userId }) => new Promise(async (resolve, reject) => {
     try {
       JSON.parse(message);
+      const author = request.user.id;
       const r = await PostsModel.create({
         message,
-        user: request.user.id,
+        author,
+        user: userId || author,
       });
+
+      if (userId && _.isEqual(userId, author)) {
+        sendPostNotification(r._id, userId);
+      }
       resolve(r);
     } catch (e) {
       reject(e);
