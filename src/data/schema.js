cbc78630c1d88678cf5b80282829d33090a8a190
Nam Hoang Van <particle4dev@gmail.com>
2017-07-19 16:10:11 +0700

    finish task edit post update BE (add edit post mutation and test)

--- src/data/schema.js
+++ src/data/schema.js
@@ -80,6 +80,10 @@ type Mutation {
     userId: String
     privacy: PrivacyType
   ): Post
+  editPost (
+    _id: String!
+    message: String!
+  ): Post
   deletePost (
     _id:String!
   ): Post
@@ -221,7 +225,7 @@ const rootResolvers = {
         status: ACCEPTED,
       }).select('friend _id');
       friendListByIds = friendListByIds.map(v => v.friend);
-      friendListByIds.push(userId);
+      // friendListByIds.push(userId);
       friendListByIds = friendListByIds.map(toObjectId);
       const r = await UsersModel.find({
         _id: { $in: friendListByIds },
@@ -392,6 +396,24 @@ const rootResolvers = {
         },
       });
       return UsersModel.findOne({ _id: userId });
+    },
+    async editPost({ request }, { _id, message }) {
+      const p = await PostsModel.findOne({
+        _id,
+      });
+      if (!p) {
+        throw new Error('not found the post');
+      }
+      await PostsModel.update({
+        _id,
+      }, {
+        $set: {
+          message,
+        },
+      });
+      return PostsModel.findOne({
+        _id,
+      });
     },
   },
 };
---
+++ src/data/tests/RootEditPostMutation.test.js
@@ -0,0 +1,66 @@
+import { graphql } from 'graphql';
+import {
+  setupTest,
+  getContext,
+} from '../../../test/helper';
+import { PostsModel } from '../models';
+import schema from '../schema';
+
+// beforeEach(async () => await setupTest());
+beforeAll(async () => await setupTest());
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+
+const postId = '58f9d6b62d4581000484b1a3';
+const userId = '58f9c2502d4581023484b18a';
+const oldMessage = '{\'entityMap\':{},\'blocks\':[{\'key\':\'4gvpl\',\'text\':\'Viet nam tuoi dep\',\'type\':\'unstyled\',\'depth\':0,\'inlineStyleRanges\':[],\'entityRanges\':[],\'data\':{}}]}';
+const newMessage = '{\'entityMap\':{},\'blocks\':[{\'key\':\'4gvpl\',\'text\':\'Reactjs VN\',\'type\':\'unstyled\',\'depth\':0,\'inlineStyleRanges\':[],\'entityRanges\':[],\'data\':{}}]}';
+
+const postData = {
+  _id: postId,
+  message: oldMessage,
+  user: userId,
+  author: userId,
+  likes: [userId],
+  photos: [],
+  type: 'STATUS',
+  __v: 0,
+};
+
+describe('RootEditPostMutation', () => {
+  beforeEach(async () => {
+    // setup db
+    const post = new PostsModel(postData);
+    await post.save();
+  });
+
+  test('edit post with message is string', async () => {
+    const query = `
+      mutation editPost { 
+        editPost(_id:"${postId}", message: "${newMessage}") {
+          _id
+          message
+        }
+      }
+    `;
+    const rootValue = {
+      request: {
+        postData: {
+          id: postId,
+        },
+      },
+    };
+    const context = getContext({});
+    const result = await graphql(schema, query, rootValue, context);
+    expect(result.data.editPost).toEqual(Object.assign({}, {
+      _id: postId,
+      message: newMessage,
+    }));
+  });
+
+  afterEach(async () => {
+    // clear data
+    await PostsModel.remove({
+      _id: postData._id,
+    });
+  });
+});
