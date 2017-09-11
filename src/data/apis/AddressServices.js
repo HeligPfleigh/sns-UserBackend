688abfd920ef8c33a599cc172426ebae12e2024c
Linh Le Duc <linh.le@mttjsc.com>
2017-09-11 17:30:26 +0700

    Implement apis for My Building Page

--- src/data/apis/AddressServices.js
+++ src/data/apis/AddressServices.js
@@ -25,8 +25,27 @@ function getApartment(apartmentId) {
   return ApartmentsModel.findOne({ _id: apartmentId });
 }
 
+function getApartmentsOfBuilding(userId, buildingId) {
+  const options = {
+    building: buildingId,
+  };
+
+  if (userId) {
+    options.$or = [
+      { owner: userId },
+      {
+        users: {
+          $in: [userId],
+        },
+      },
+    ];
+  }
+  return ApartmentsModel.find(options).sort({ name: 1 });
+}
+
 export default {
   getBuilding,
   getBuildings,
   getApartment,
+  getApartmentsOfBuilding,
 };
